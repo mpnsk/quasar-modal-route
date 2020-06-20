ef869df50178c7412656f5240f563d153af470ef
user <>
2020-06-20 16:47:39 +0200

    Delete verbose logs

--- src/components/MyDialog.vue
+++ src/components/MyDialog.vue
@@ -92,43 +92,13 @@
       }
     },
     beforeRouteEnter (to, from, next) {
-      console.log("beforeRouteEnter")
-      console.log("to:" + to)
-      console.log(to)
-      console.log("from:" + from)
-      console.log(from)
-      // console.log("next:" + next)
       next()
-      // called before the route that renders this component is confirmed.
-      // does NOT have access to `this` component instance,
-      // because it has not been created yet when this guard is called!
     },
     beforeRouteUpdate (to, from, next) {
-      console.log("beforeRouteUpdate")
-      console.log("to:" + to)
-      console.log(to)
-      console.log("from:" + from)
-      console.log(from)
-      // console.log("next:" + next)
       next()
-      // called when the route that renders this component has changed,
-      // but this component is reused in the new route.
-      // For example, for a route with dynamic params `/foo/:id`, when we
-      // navigate between `/foo/1` and `/foo/2`, the same `Foo` component instance
-      // will be reused, and this hook will be called when that happens.
-      // has access to `this` component instance.
     },
     beforeRouteLeave (to, from, next) {
-      console.log("beforeRouteLeave")
-      console.log("to:" + to)
-      console.log(to)
-      console.log("from:" + from)
-      console.log(from)
-      // console.log("next:" + next)
       next()
-      // called when the route that renders this component is about to
-      // be navigated away from.
-      // has access to `this` component instance.
     }
   }
 </script>
